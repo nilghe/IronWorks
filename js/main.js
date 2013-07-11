73d9274c86e6b6c8ccf4ed57bf2bd4286b9ad032
Chris Nilghe <chris@nilghe.com>
2013-07-11 16:38:36 -0600

    Initial Commit of the project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  /* Flex Slider Init */
+   $('.flexslider').flexslider({
+       animation: "slide",
+       controlNav: false
+   });
+
+    /* Animate scrolling to anchors */
+    $('.nav > li > a, #footer-menu > li > a').click(function(){
+        var anchor = $(this).attr('href');
+        var height = $('.navbar').height(); //offset by the fixed nav bar
+        $('html, body').animate({
+           scrollTop: $(anchor).offset().top - height
+        }, 400);
+        return false;
+    });
+});
