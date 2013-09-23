951109a7828638e94739348da10ffe3e6ffb6c66
Chris Nilghe <chris@nilghe.com>
2013-09-23 16:45:04 -0600

    Contact Form Boilerplate
    Put in the boilerplate for the contact form. Need to do the PHP script.

--- js/main.js
+++ js/main.js
@@ -25,4 +25,41 @@ $(document).ready(function(){
     		$(this).removeClass('animated bounce');
     	}
     );
+
+    // Contact Form
+    $('#submit-email').on('click', function(e){
+        
+        var name = $('#input-name').val();
+        var number = $('#input-number').val();
+        var email = $('#input-email').val();
+        var subject = $('#input-subject').val();
+        var message = $('#input-message').val();
+
+        console.log(name);
+        console.log(number);
+        console.log(email);
+        console.log(subject);
+        console.log(message);
+        
+        var queryString = 'name=' + name
+                        + '&number=' + number
+                        + '&email=' + email
+                        + '&subject=' + subject
+                        + '&message=' + message;
+
+        console.log(queryString);
+
+        $.ajax({
+            type: "POST",
+            url: "../contact/submit.php",
+            data: queryString,
+            success: function(){
+                console.log('email sent!');
+            },
+            error: function(){
+                console.log('failed');
+            } 
+        });
+        e.preventDefault();
+    });
 });
