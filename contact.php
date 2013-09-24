457b38da573806c776126cfff5a07d725c034fcf
Chris Nilghe <chris@nilghe.com>
2013-09-24 14:48:22 -0600

    Validation & CSS3 Transitions
    Added validation, error/success messages, and CSS3 transitions. Tested
    and ready to merge into main branch.

--- contact.php
+++ contact.php
@@ -1,3 +1,10 @@
+<!--	
+  Chris Nilghe - http://www.nilghe.com
+  Iron Technologies	Contact Form
+  09-24-2013
+  Source: https://spruce.it/noise/simple-ajax-contact-form/	
+-->
+
 <?php
 	if (isset($_POST['name']) &&
 		isset($_POST['number']) &&
@@ -5,9 +12,22 @@
 		isset($_POST['message']) &&
 		filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
 
+		$message = 	"Name: " . $_POST['name'] . "\n" .
+					"Phone Number: " . $_POST['number'] . "\n" .
+					"In reference to: " . $_POST['subject'] . "\n" .
+					"Message: " . $_POST['message'];
+
+		// Prevent header injections
+		$check = "/(content-type|bcc:|cc:|to:)/i";
+		foreach ($_POST as $key => $value) {
+			if(preg_match($check, $value)) {
+				exit;
+			}
+		}
+
 		mail("burnin.lover@gmail.com", 
-			 "Contact From: " . $_POST['name'],
-			 $_POST['message'],
+			 "Iron Technologies Contact From: " . $_POST['name'],
+			 $message,
 			 "From: " . $_POST['email']);
 	}
 ?>
--- js/main.js
+++ js/main.js
@@ -35,11 +35,7 @@ $(document).ready(function(){
         var subject = $('#input-subject').val();
         var message = $('#input-message').val();
 
-        console.log(name);
-        console.log(number);
-        console.log(email);
-        console.log(subject);
-        console.log(message);
+        var submitBtn = $(this);
         
         var queryString = 'name=' + name
                         + '&number=' + number
@@ -47,19 +43,32 @@ $(document).ready(function(){
                         + '&subject=' + subject
                         + '&message=' + message;
 
-        console.log(queryString);
+        if (validEmail(email)) {
+            $.ajax({
+                type: "POST",
+                url: "contact.php",
+                data: queryString,
+                success: function(){
+                    console.log('email sent!');
+                    submitBtn.addClass('btn-success disabled');
+                    submitBtn.html('<i class="icon-ok" style="font-size: 1.1em;"></i> Email was sent!');
+                },
+                error: function(){
+                    console.log('failed');
+                    submitBtn.addClass('btn-danger');
+                    submitBtn.html('<i class="icon-frown" style="font-size: 1.1em;"></i> Email not sent, try again.');
+                } 
+            });
+        }
 
-        $.ajax({
-            type: "POST",
-            url: "contact.php",
-            data: queryString,
-            success: function(){
-                console.log('email sent!');
-            },
-            error: function(){
-                console.log('failed');
-            } 
-        });
         e.preventDefault();
     });
+
+    /*  ==================================
+        Helper Functions
+        ================================== */
+    function validEmail(email) {
+        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        return pattern.test(email);
+    }
 });
