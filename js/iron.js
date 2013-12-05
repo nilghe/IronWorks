19fe5fd12163859a371327acf223bc586c31f4cd
Chris Nilghe <chris@nilghe.com>
2013-12-05 14:17:02 +0900

    Improving logo text & adding js for hiding/showing text
    The logo for the site is just going to be text for now. Created some
    media queries to help scale the text properly.
    
    Since "Iron Technologies" also shows up in the navigation bar. Created
    a simple script that will detect when the main logo contain is in the
    viewport. When it's in the viewport the text in the navigation bar will
    be hidden. Once the main logo is out of the view port then the text in
    the navbar will display.
    
    This is to reduce redundant text that could confuse the user.

--- js/iron.js
+++ js/iron.js
@@ -3,6 +3,28 @@
 
 $(document).ready(function(){
 
+    /* Hide the brand text since it's the same text as the main logo. 
+     * This way there isn't repeating elements on the screen
+     * JavaScript is used instead just in case the browser the user has does not support javascript.
+     * Then the logo will just appear twice instead of not at all
+     */ 
+    $('.brand').hide();
+
+    // Show and Hide the main logo text and navbar text based on the viewport
+    $(window).scroll(function() {
+        var mainTextLogo = $('#main-logo-txt');
+
+        // If the main logo is in viewport then hide the 'brand' text in the nav bar
+        // Once the main logo is not in the viewport then show the 'brand' text
+        if (isScrolledOutOfView(mainTextLogo)){
+            $('.brand').fadeIn('slow');
+        }
+        else
+        {
+            $('.brand').fadeOut('slow');
+        }
+    });
+
     // Animate scrolling to anchors 
     $('.scroll-to').click(function(){
         var anchor = $(this).attr('href');
@@ -73,5 +95,23 @@ $(document).ready(function(){
     function validEmail(email) {
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         return pattern.test(email);
+    }
+
+    function isScrolledOutOfView(elem)
+    {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        // Return false if the element is still visible in the viewport
+        if ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && 
+            (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop)) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 });
